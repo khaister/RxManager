6f169dc1857c251c82eb892b0e724015ba842447
Khai Nguyen <nguyenkhai101@gmail.com>
2015-04-06 22:26:36 -0700

    Added appropriate attributes

---
+++ src/doctor/backend/Patient.java
@@ -0,0 +1,181 @@
+package doctor.backend;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+/**
+ * This class represents a patient with identificable information, including
+ * name, date of birth, phone number, address, and medical number.
+ * @author khaister
+ *
+ */
+public class Patient
+{
+	private String pFirstName;
+	private String pLastName;
+	private Date pDOB;
+	private String pPhone;
+	private String pAddress;
+	private String pCity;
+	private String pState;
+	private String pZipCode;
+	private String pMedicalNumber;
+
+	/**
+	 * Creates an "empty" patient.
+	 */
+	public Patient()
+	{
+		this("", "", new Date(), "", "", "", "", "", "");
+	}
+
+	
+	/**
+	 * Creates a Patient object with all identifiable information.
+	 * @param fname First name of the patient.
+	 * @param lname Last name of the patient.
+	 * @param dob Date of birth of the patient.
+	 * @param phone Phone number of the patient.
+	 * @param address Address, including number and street name.
+	 * @param city City.
+	 * @param state State.
+	 * @param zipcode Zip code.
+	 * @param medicalNumber Medical number.
+	 */
+	public Patient(String fname, String lname, Date dob, String phone,
+			String address, String city, String state, String zipcode,
+			String medicalNumber)
+	{
+		this.pFirstName = fname;
+		this.pLastName = lname;
+		this.pDOB = dob;
+		this.pPhone = phone;
+		this.pAddress = address;
+		this.pCity = city;
+		this.pState = state;
+		this.pZipCode = zipcode;
+		this.pMedicalNumber = medicalNumber;
+	}
+	
+	////////////////////////////////////////////////////////////////////////////
+	//
+	//                          SETTERS / MUTATORS
+	//
+	////////////////////////////////////////////////////////////////////////////
+	public void setFirstName(String fname)
+	{
+		this.pFirstName = fname;
+		
+	}
+	
+	public void setLastName(String lname)
+	{
+		this.pLastName = lname;	
+	}
+	
+	public void setDOB(Date dob)
+	{
+		this.pDOB = dob;
+	}
+	
+	public void setPhone(String phone)
+	{
+		this.pPhone = phone;
+	}
+	
+	public void setAddress(String address)
+	{
+		this.pAddress = address;
+	}
+	
+	public void setCity(String city)
+	{
+		this.pCity = city;
+	}
+	
+	public void setState(String state)
+	{
+		this.pState = state;
+	}
+	
+	public void setZipCode(String zipcode)
+	{
+		this.pZipCode = zipcode;
+	}
+	
+	public void setMedicalNumber(String medical_number)
+	{
+		this.pMedicalNumber = medical_number;
+	}
+	
+	public void setDocFirstName(String docfname)
+	{
+		this.docFirstName = docfname;
+	}
+	
+	public void setDocLastName(String doclname)
+	{
+		this.docLastName = doclname;
+	}
+	
+	public void setDocLicense(String license)
+	{
+		this.docLicense = license;
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+	//
+	//                          GETTERS / ACCESSORS
+	//
+	////////////////////////////////////////////////////////////////////////////
+	public String getFirstName()
+	{
+		return this.pFirstName;
+	}
+	public String getLastName()
+	{
+		return this.pLastName;
+	}
+	public Date getDOB()
+	{
+		return this.pDOB;
+	}
+	public String getPhone()
+	{
+		return this.pPhone;
+	}
+	public String getAddress()
+	{
+		return this.pAddress;
+	}
+	public String getCity()
+	{
+		return this.pCity;
+	}
+	public String getState()
+	{
+		return this.pState;
+	}
+	public String getZipCode()
+	{
+		return this.pZipCode;
+	}
+	public String getMedicalNumber()
+	{
+		return this.pMedicalNumber;
+	}
+	public String getDocFirtName()
+	{
+		return this.docFirstName;
+	}
+	public String getDocLastName()
+	{
+		return this.docLastName;
+	}
+	public String getDocLicense()
+	{
+		return this.docLicense;
+	}
+	
+
+}
