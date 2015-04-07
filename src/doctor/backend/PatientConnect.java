e2863571064087fcea084f9277ecd41b3838f977
Khai Nguyen <nguyenkhai101@gmail.com>
2015-04-07 10:40:42 -0700

    Added methods for retrieval, inserting, & updating rows in the db

--- src/doctor/backend/PatientConnect.java
+++ src/doctor/backend/PatientConnect.java
@@ -29,6 +29,28 @@ public class PatientConnect
 
 	/** The name of the database we are testing with (this default is installed with MySQL) */
 	private final String dbName = "rxmanagerdb";
+	
+	/** Connection to the MySQL database */
+	private Connection conn;
+	
+	/**
+	 * Creates a PatientConnect instance and connect to the database.
+	 */
+	public PatientConnect()
+	{
+		// Connect to MySQL
+		try 
+		{
+			conn = this.getConnection();
+			//System.out.println("Connected to database");
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("ERROR: Could not connect to the database!");
+			e.printStackTrace();
+			//return;
+		}
+	}
 
 
 	/**
@@ -61,28 +83,15 @@ public class PatientConnect
 	public ArrayList<Patient> getPatient(String fname, String lname) 
 	{
 		ArrayList<Patient> foundPatients = new ArrayList<Patient>();
-
-		// Connect to MySQL
-		Connection conn = null;
-		try 
-		{
-			conn = this.getConnection();
-			//System.out.println("Connected to database");
-		} 
-		catch (SQLException e) 
-		{
-			System.out.println("ERROR: Could not connect to the database!");
-			e.printStackTrace();
-			//return;
-		}
-
+		
+		String sql = "SELECT * FROM Patients p "
+				+ "WHERE p.pFirstName = " + fname
+				+ "AND p.pLastName = " + lname;
+		
 		// Retrieve a record from the table Patients
 		try
 		{
 			Statement st = conn.createStatement();
-			String sql = "SELECT * FROM Patients p "
-					+ "WHERE p.pFirstName = " + fname
-					+ "AND p.pLastName = " + lname;
 			ResultSet rs = st.executeQuery(sql);
 
 			while (rs.next()) // moving the cursor forward throu the rows
@@ -108,6 +117,71 @@ public class PatientConnect
 		}
 		
 		return foundPatients;
+	}
+	
+	
+	/**
+	 * Inserts a patient record into Patients table. It is assumed that there
+	 * is no record for this patient exists yet.
+	 * @param p Patient to be inserted.
+	 * @return 0 if inserted sucessfully, 1 otherwise.
+	 */
+	public int createPatientRecord(Patient p)
+	{
+		int errorCode = 0;
+		String sql = "INSERT INTO Patients VALUES " + p.toSQLInsertString();
+		
+		try
+		{
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			rs.close();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			errorCode = 1;
+		}
+
+		return errorCode;
+	}
+	
+	public int modifyPatientRecord(Patient oldRecord, Patient newRecord)
+	{
+		int errorCode = 0;
+		
+		String sql = "SELECT * FROM Patients p "
+				+ "WHERE p.pFirstName = " + oldRecord.getFirstName()
+				+ "AND p.pLastName = " + oldRecord.getLastName()
+				+ "AND p.pPhone = " + oldRecord.getPhone();
+		
+		try
+		{
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+			// This should return only 1 row.
+			while (rs.next())
+			{
+				rs.updateString("pFirstName", newRecord.getFirstName());
+				rs.updateString("pLastName", newRecord.getLastName());
+				rs.updateDate("pDOB", newRecord.getDOB());
+				rs.updateString("pAddress", newRecord.getAddress());
+				rs.updateString("pCity", newRecord.getCity());
+				rs.updateString("pState", newRecord.getState());
+				rs.updateString("pZipCode", newRecord.getZipCode());
+				rs.updateString("pMedicalNumber", newRecord.getMedicalNumber());
+			}
+			
+			rs.close();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			errorCode = 1;
+		}
+
+		return errorCode;
 	}
 
 }
