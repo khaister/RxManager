f97c3627328cf1b2f52c58a1022f1785023cb35c
Khai Nguyen <nguyenkhai101@gmail.com>
2015-04-23 09:47:49 -0700

    Phasing out DBConnect
    Methods in this class are slowly integrated into other classes as needed

--- src/common/DBConnect.java
+++ src/common/DBConnect.java
@@ -1,14 +1,14 @@
 package common;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+//import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 import java.sql.ResultSet;
-import java.util.ArrayList;
+//import java.util.Properties;
+//import java.util.ArrayList;
+
 import doctor.backend.*;
-import pharmacist.backend.*;
 
 /**
  * 
@@ -17,136 +17,136 @@ import pharmacist.backend.*;
  */
 public class DBConnect
 {
-	/** The name of the MySQL account to use (or empty for anonymous) */
-	private final String userName = "rxmandoc";
-
-	/** The password for the MySQL account (or empty for anonymous) */
-	private final String password = "rxmandocpasswd";
-
-	/** The name of the computer running MySQL */
-	private final String serverName = "localhost";
-
-	/** The port of the MySQL server (default is 3306) */
-	private final int portNumber = 3306;
-
-	/** The name of the database we are testing with (this default is installed with MySQL) */
-	private final String dbName = "rxmanagerdb";
-	
-	/** Connection to the MySQL database */
+//	/** The name of the MySQL account to use (or empty for anonymous) */
+//	private final String userName = "rxmandoc";
+//
+//	/** The password for the MySQL account (or empty for anonymous) */
+//	private final String password = "rxmandocpasswd";
+//
+//	/** The name of the computer running MySQL */
+//	private final String serverName = "localhost";
+//
+//	/** The port of the MySQL server (default is 3306) */
+//	private final int portNumber = 3306;
+//
+//	/** The name of the database we are testing with (this default is installed with MySQL) */
+//	private final String dbName = "rxmanagerdb";
+//	
+//	/** Connection to the MySQL database */
 	private Connection conn;
+//	
+//	/**
+//	 * Creates a PatientConnect instance and connect to the database.
+//	 */
+//	public DBConnect()
+//	{
+//		// Connect to MySQL
+//		try 
+//		{
+//			conn = this.getConnection();
+//			//System.out.println("Connected to database");
+//		} 
+//		catch (SQLException e) 
+//		{
+//			System.out.println("ERROR: Could not connect to the database!");
+//			e.printStackTrace();
+//			//return;
+//		}
+//	}
+
+
+//	/**
+//	 * Gets a new database connection.
+//	 * 
+//	 * @return An object represents the database connection.
+//	 * @throws SQLException
+//	 */
+//	public Connection getConnection() throws SQLException 
+//	{
+//		Connection conn = null;
+//		Properties connectionProps = new Properties();
+//		connectionProps.put("user", this.userName);
+//		connectionProps.put("password", this.password);
+//
+//		conn = DriverManager.getConnection("jdbc:mysql://"
+//				+ this.serverName + ":" + this.portNumber + "/" + this.dbName,
+//				connectionProps);
+//
+//		return conn;
+//	}
+
 	
-	/**
-	 * Creates a PatientConnect instance and connect to the database.
-	 */
-	public DBConnect()
-	{
-		// Connect to MySQL
-		try 
-		{
-			conn = this.getConnection();
-			//System.out.println("Connected to database");
-		} 
-		catch (SQLException e) 
-		{
-			System.out.println("ERROR: Could not connect to the database!");
-			e.printStackTrace();
-			//return;
-		}
-	}
-
-
-	/**
-	 * Gets a new database connection.
-	 * 
-	 * @return An object represents the database connection.
-	 * @throws SQLException
-	 */
-	public Connection getConnection() throws SQLException 
-	{
-		Connection conn = null;
-		Properties connectionProps = new Properties();
-		connectionProps.put("user", this.userName);
-		connectionProps.put("password", this.password);
-
-		conn = DriverManager.getConnection("jdbc:mysql://"
-				+ this.serverName + ":" + this.portNumber + "/" + this.dbName,
-				connectionProps);
-
-		return conn;
-	}
-
-	
-	/**
-	 * Looks up patients by both first name and last name.
-	 * @param fname First name.
-	 * @param lname Last name.
-	 * @return An ArrayList of patients with the given first and last name.
-	 */
-	public ArrayList<Patient> getPatient(String fname, String lname) 
-	{
-		ArrayList<Patient> foundPatients = new ArrayList<Patient>();
-		
-		String sql = "SELECT * FROM Patients p "
-				+ "WHERE p.pFirstName = " + fname
-				+ "AND p.pLastName = " + lname;
-		
-		// Retrieve a record from the table Patients
-		try
-		{
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-
-			while (rs.next()) // moving the cursor forward throu the rows
-			{
-				Patient p = new Patient();
-				p.setFirstName(rs.getString("pFirstName"));
-				p.setLastName(rs.getString("pLastName"));
-				p.setDOB(rs.getDate("pDOB"));
-				p.setPhone(rs.getString("pPhone"));
-				p.setAddress(rs.getString("pAddress"));
-				p.setCity(rs.getString("pCity"));
-				p.setState(rs.getString("pState"));
-				p.setZipCode(rs.getString("pZipCode"));
-				p.setMedicalNumber(rs.getString("pMedicalNumber"));
-				
-				foundPatients.add(p);
-			}
-			rs.close();
-		} 
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return foundPatients;
-	}
+//	/**
+//	 * Looks up patients by both first name and last name.
+//	 * @param fname First name.
+//	 * @param lname Last name.
+//	 * @return An ArrayList of patients with the given first and last name.
+//	 */
+//	public ArrayList<Patient> getPatient(String fname, String lname) 
+//	{
+//		ArrayList<Patient> foundPatients = new ArrayList<Patient>();
+//		
+//		String sql = "SELECT * FROM Patients p "
+//				+ "WHERE p.pFirstName = " + fname
+//				+ "AND p.pLastName = " + lname;
+//		
+//		// Retrieve a record from the table Patients
+//		try
+//		{
+//			Statement st = conn.createStatement();
+//			ResultSet rs = st.executeQuery(sql);
+//
+//			while (rs.next()) // moving the cursor forward throu the rows
+//			{
+//				Patient p = new Patient();
+//				p.setFirstName(rs.getString("pFirstName"));
+//				p.setLastName(rs.getString("pLastName"));
+//				p.setDOB(rs.getDate("pDOB"));
+//				p.setPhone(rs.getString("pPhone"));
+//				p.setAddress(rs.getString("pAddress"));
+//				p.setCity(rs.getString("pCity"));
+//				p.setState(rs.getString("pState"));
+//				p.setZipCode(rs.getString("pZipCode"));
+//				p.setMedicalNumber(rs.getString("pMedicalNumber"));
+//				
+//				foundPatients.add(p);
+//			}
+//			rs.close();
+//		} 
+//		catch (SQLException e)
+//		{
+//			e.printStackTrace();
+//		}
+//		
+//		return foundPatients;
+//	}
 	
 	
-	/**
-	 * Inserts a patient record into Patients table. It is assumed that there
-	 * is no record for this patient exists yet.
-	 * @param p Patient to be inserted.
-	 * @return 0 if inserted sucessfully, 1 otherwise.
-	 */
-	public int createPatientRecord(Patient p)
-	{
-		int errorCode = 0;
-		String sql = "INSERT INTO Patients VALUES " + p.toSQLInsertString();
-		
-		try
-		{
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			rs.close();
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-			errorCode = 1;
-		}
-
-		return errorCode;
-	}
+//	/**
+//	 * Inserts a patient record into Patients table. It is assumed that there
+//	 * is no record for this patient exists yet.
+//	 * @param p Patient to be inserted.
+//	 * @return 0 if inserted sucessfully, 1 otherwise.
+//	 */
+//	public int createPatientRecord(Patient p)
+//	{
+//		int errorCode = 0;
+//		String sql = "INSERT INTO Patients VALUES " + p.toSQLInsertString();
+//		
+//		try
+//		{
+//			Statement st = conn.createStatement();
+//			ResultSet rs = st.executeQuery(sql);
+//			rs.close();
+//		}
+//		catch (SQLException e)
+//		{
+//			e.printStackTrace();
+//			errorCode = 1;
+//		}
+//
+//		return errorCode;
+//	}
 	
 	/**
 	 * Updates a patient record. Note: all old info will be overwritten with
@@ -194,89 +194,31 @@ public class DBConnect
 		return errorCode;
 	}
 	
-	/**
-	 * Creates a new Prescription record (row) in the database.
-	 * 
-	 * @param rx Prescription to be added to the database.
-	 * @return 0 if the INSERT is sucessful, 1 otherwise.
-	 */
-	public int sendPrescription(Prescription rx)
-	{
-		int errorCode = 0;
-		
-		String sql = "INSERT INTO Prescriptions VALUES " + rx.toSQLInsertString();
-		
-		try
-		{
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			rs.close();
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-			errorCode = 1;
-		}
-		
-		return errorCode;
-	}
-	
-	/**
-	 * Checsk whether the credential entered by user is correct or not.
-	 *  
-	 * @param username
-	 * @param password
-	 * @return true if the credential matches that in the database, false otherwise.
-	 */
-	public boolean userAuthentication(String username, String password)
-	{
-		boolean isCorrectID = false;
-		String sql = "SELECT dPassword FROM Doctors WHERE dUsername = " + username;
-		
-		try
-		{
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			
-			rs.first(); // go to the first row in the result set (should only have one row)
-			if (rs.getString("dPassword") == password)
-				isCorrectID = true;
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return isCorrectID;
-	}
-	
-	/**
-	 * Obtains the doctor's record from the database.
-	 * @param username Username of the doctor.
-	 * @return The Doctor object that matches the username.
-	 */
-	public Doctor getDoctorRecord(String username)
-	{
-		String sql = "SELECT * FROM Doctors WHERE dUsername = " + username;
-		Doctor foundDoc = null;
-		
-		try
-		{
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			
-			rs.first();
-			foundDoc = new Doctor(rs.getString("dFirstName"), 
-					rs.getString("dLastName"), rs.getDate("dDOB"), 
-					rs.getString("dLicense"), rs.getString("dUserName"),
-					rs.getString("dPassword"));
-			
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return foundDoc;
-	}
+//	/**
+//	 * Creates a new Prescription record (row) in the database.
+//	 * 
+//	 * @param rx Prescription to be added to the database.
+//	 * @return 0 if the INSERT is sucessful, 1 otherwise.
+//	 */
+//	public int sendPrescription(Prescription rx)
+//	{
+//		int errorCode = 0;
+//		
+//		String sql = "INSERT INTO Prescriptions VALUES " + rx.toSQLInsertString();
+//		
+//		try
+//		{
+//			Statement st = conn.createStatement();
+//			ResultSet rs = st.executeQuery(sql);
+//			rs.close();
+//		}
+//		catch (SQLException e)
+//		{
+//			e.printStackTrace();
+//			errorCode = 1;
+//		}
+//		
+//		return errorCode;
+//	}
 
 }
