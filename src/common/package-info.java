b7436413cbfe3d5c4170118944a9f591e6a99c1b
Khai Nguyen <nguyenkhai101@gmail.com>
2015-04-28 09:55:47 -0700

    added preliminary description

---
+++ src/common/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains several classes that are used by both the doctor and
+ * pharmacy clients. Particularly, it has Prescription, User, and Window classes.
+ */
+package common;
